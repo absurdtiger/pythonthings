2cf995a25213c8aa834580ef00d449955d220ed6
absurdtiger <jodietheteo@gmail.com>
2022-02-22 14:20:01 +0800

    Create notes8.py

---
+++ realpython/chapter_8/notes8.py
@@ -0,0 +1,7 @@
+# unconditional code is executed in the order it is written
+# conditional logic performs different actions based on different conditions
+# paired with functions and loops
+
+### 8.1 compare values ###
+
+
